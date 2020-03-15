858d83694d79c8c07f6f62aa69a5b547f14ba765
KatsuroDev <katsuro.userg@gmail.com>
2020-03-15 12:00:47 -0400

    Need to commit to keep changes

--- src/JustArenderTest.h
+++ src/JustArenderTest.h
@@ -1,14 +1,15 @@
 #ifndef JUSTARENDERTEST_H
 #define JUSTARENDERTEST_H
 
-#include <SOIL.h>
+#include <GL/glew.h>
+#include <GL/SOIL.h>
 
 class somethingStatic
 {
 public:
   static void staticMain();
 private:
-  
+
 };
 
 #endif
